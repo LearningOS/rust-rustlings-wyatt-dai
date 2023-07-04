49daae8e3e3ae11a0128a60163cec6df53952a08
wyatt-dai <138414578+wyatt-dai@users.noreply.github.com>
2023-07-04 10:49:53 +0800

    Add files via upload

---
+++ exercises/clippy/target/debug/deps/clippy3-96539de3c0b15af6.d
@@ -0,0 +1,8 @@
+C:\Users\John_\rustlings\exercises\clippy\target\debug\deps\clippy3-96539de3c0b15af6.rmeta: clippy3.rs Cargo.toml
+
+C:\Users\John_\rustlings\exercises\clippy\target\debug\deps\clippy3-96539de3c0b15af6.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
